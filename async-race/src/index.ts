332e594a09e3a17c5aa0e95324112a9b15577dd1
Maria Kuznetsova <makuza.mk@gmail.com>
2025-04-19 13:43:08 +0300

    feat: add drive_car.ts, buttons_garag.ts, ui.ts, update index.ts

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,3 +1,6 @@
 import './styles.css';
 import './components/garage/api_garage';
 import './components/garage/utils_garage';
+import './components/garage/buttons_garage';
+import './components/ui';
+import './components/garage/drive_car';
